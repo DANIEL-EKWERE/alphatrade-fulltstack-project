6ea089f134279ddac94e219843544b25103de511
DANIEL-EKWERE <ekweredaniel8@gmail.com>
2024-11-23 15:40:03 +0100

    alphatrade update7

--- alphatrade/settings.py
+++ alphatrade/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$$riv5w)o1_966i6n((c%=*$ny1f-@a+%xn6y2b+==4p**p-)w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alphatrade.onrender.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['alphatrade.onrender.com','127.0.0.1:8000','*']
 
 
 # Application definition
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
